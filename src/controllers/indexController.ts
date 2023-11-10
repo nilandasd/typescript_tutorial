7d9760a737b260212c1e757594fa404a47f8f4d5
Lando <naschuma@ucsc.edu>
2023-11-10 14:13:08 -0800

    got debuging working

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 
 class IndexController {
     show(req: Request, res: Response, next: NextFunction) {
+      // tslint:disable-next-line:no-debugger
+      debugger;
       res.render("index");
     }
 }
