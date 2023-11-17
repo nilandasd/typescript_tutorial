175a0b6a52761ee6bafd03b942d87a13819be459
Lando <naschuma@ucsc.edu>
2023-11-17 08:28:17 -0800

    removed unnecessary codebuild config

--- terraform/buildproject.tf
+++ terraform/buildproject.tf
@@ -28,44 +28,6 @@ data "aws_iam_policy_document" "build_project_policy_document" {
 
     resources = ["*"]
   }
-
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "ec2:CreateNetworkInterface",
-      "ec2:DescribeDhcpOptions",
-      "ec2:DescribeNetworkInterfaces",
-      "ec2:DeleteNetworkInterface",
-      "ec2:DescribeSubnets",
-      "ec2:DescribeSecurityGroups",
-      "ec2:DescribeVpcs",
-    ]
-
-    resources = ["*"]
-  }
-
-  statement {
-    effect    = "Allow"
-    actions   = ["ec2:CreateNetworkInterfacePermission"]
-    resources = ["arn:aws:ec2:us-east-1:123456789012:network-interface/*"]
-
-    condition {
-      test     = "StringEquals"
-      variable = "ec2:Subnet"
-
-      values = [
-        aws_subnet.private_us_east_1a.arn,
-        aws_subnet.private_us_east_1b.arn,
-      ]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "ec2:AuthorizedService"
-      values   = ["codebuild.amazonaws.com"]
-    }
-  }
 }
 
 resource "aws_iam_role_policy" "build_project_policy" {
@@ -82,21 +44,10 @@ resource "aws_codebuild_project" "project-with-cache" {
     type = "CODEPIPELINE"
   }
 
-  cache {
-    type  = "LOCAL"
-    modes = ["LOCAL_DOCKER_LAYER_CACHE"]
-  }
-
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
     image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
     type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "CODEBUILD"
-
-    environment_variable {
-      name  = "SOME_KEY1"
-      value = "SOME_VALUE1"
-    }
   }
 
   source {
